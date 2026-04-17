fc283485a2adbee6e9895076fe87355f817e3020
_drazy <48094316+Deadboy666@users.noreply.github.com>
2026-04-17 01:01:40 -0500

    Create plsfindmyconfigdir.sh

---
+++ plsfindmyconfigdir.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -eu
+
+SLSsteamConfigDir=$HOME/.config/SLSsteam
+FlatpakSLSsteamConfigDir=$HOME/.var/app/com.valvesoftware.Steam/.config/SLSsteam
+
+    OpenSLSsteamDir(){
+            if [ -d "$FlatpakSLSsteamConfigDir" ]; then
+                xdg-open $FlatpakSLSsteamConfigDir/
+            else
+                    xdg-open $SLSsteamConfigDir/
+                fi
+                    echo "" &> /dev/null
+                }
+
+OpenSLSsteamDir
